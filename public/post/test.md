28c596b43637c4985c35be69093811da2793f980
Max <maxzqiu@gmail.com>
2026-08-21 10:28:58 -0700

    restored website

---
+++ public/post/test.md
@@ -0,0 +1,8 @@
+---
+title: Cursor Balls
+link: https://www.cursor-balls.vercel.app
+tags: one, two, three
+summary: Have some fun
+---
+
+Max is fun.
---
+++ src/markdownParser.jsx
@@ -0,0 +1,92 @@
+//import raw from "../public/post/test.md?raw";
+
+function parseOneStar(str) {
+  str = str.split("*");
+  return str.map((k, key) => (key % 2 === 1 ? <em key={key}>{k}</em> : k));
+}
+
+function parseTwoStars(str) {
+  str = str.split("**");
+  return str.map((k, key) =>
+    key % 2 === 1 ? (
+      <strong key={key}>{parseOneStar(k)}</strong>
+    ) : (
+      parseOneStar(k)
+    ),
+  );
+}
+
+function parseThreeStars(str) {
+  str = str.split("***");
+  return str.map((k, key) =>
+    key % 2 === 1 ? (
+      <strong key={key}>
+        <em>{parseTwoStars(k)}</em>
+      </strong>
+    ) : (
+      parseTwoStars(k)
+    ),
+  );
+}
+
+function parseMarkdown(str) {
+  let strings = str.split(/\r\n\r\n|\n\n/);
+  
+
+  return strings.map((str, key) => {
+    if (str.substring(0, 2) === "##") {
+      str = str.substring(2, str.length);
+      return <h2 key={key}>{str}</h2>;
+    } else if (str.charAt(0) === "#") {
+      str = str.substring(1, str.length);
+      return <h1 key={key}>{str}</h1>;
+    } else {
+      return <p>{parseThreeStars(str)}</p>;
+    }
+  });
+}
+
+function splitOnFirst(str, splitAt) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str.charAt(i) === splitAt) {
+      return [str.substring(0, i), str.substring(i + 1, str.length)];
+    }
+  }
+}
+
+function parseMetadata(str) {
+  let metadata = {};
+  
+  str = str.trim();
+  str = str.split(/\r\n|\n/);
+
+  
+  for (let i = 0; i < str.length; i += 1) {
+    let line = splitOnFirst(str[i], ":");
+    if (line[0] === "tags") {
+      line[1] = line[1].split(",");
+    }
+    metadata[line[0]] = line[1];
+  }
+  return metadata;
+}
+
+function markdownParser(raw) {
+  console.log(raw);
+  let text = raw.split("---");
+  // text[0] is empty, text[1] has metadata, and text[2] has content
+
+  
+
+  return [parseMetadata(text[1]), parseMarkdown(text[2])];
+}
+
+export default markdownParser;
+
+// {
+//   "header":<h1>Cursor Balls</h1>,
+//   "description":<p>Have some fun</p>,
+//   "tags":[<p>one</p>,<p>two</p>,<p>three</p>],
+//   "link":<a href="https://www.cursor-balls.vercel.app">https://www.cursor-balls.vercel.app</a>,
+//   "image":"/panda-clipart.jpg"
+// }
